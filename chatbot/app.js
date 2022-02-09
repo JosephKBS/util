90f7fd91148047b098daf1c9f4a6fd0a0e9637cf
Joseph <59439026+JosephKBS@users.noreply.github.com>
2022-02-09 16:49:59 -0500

    Update app.js

--- chatbot/app.js
+++ chatbot/app.js
@@ -47,8 +47,7 @@ class Chatbox {
         this.messages.push(msg1);
         
         // 원래 이 자리에 이거 있었음 'http://127.0.0.1:5000/predict -> local host'
-        fetch( 
-            input: $SCRIPT_ROOT + '/predict', {  // predict function in app.py   // SCRIPT_ROOT is defined in HTML bottom!!!
+        fetch( $SCRIPT_ROOT + '/predict',{  // predict function in app.py   // SCRIPT_ROOT is defined in HTML bottom!!!
             method: 'POST',
             body: JSON.stringify({ message: text1 }), // make it as string
             mode: 'cors',
