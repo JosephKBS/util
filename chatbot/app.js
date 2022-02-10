94a899fc8a72b523c2ed68fbabde58700ce6188c
Joseph <59439026+JosephKBS@users.noreply.github.com>
2022-02-09 21:53:25 -0500

    Update app.js

--- chatbot/app.js
+++ chatbot/app.js
@@ -1,24 +1,24 @@
 class Chatbox {
     constructor() {
-        this.args = {
-            openButton: document.querySelector('.chatbox__button'),
-            chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button')
+        this.args = { // these are class name in HTML template. Names should be equal.
+            openButton: document.querySelector('.chatbox__button'), // open, close chat box
+            chatBox: document.querySelector('.chatbox__support'),   
+            sendButton: document.querySelector('.send__button') // sending message 
         }
 
-        this.state = false;
+        this.state = false; // default = close the box
         this.messages = [];
     }
 
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
+        const {openButton, chatBox, sendButton} = this.args; // arguments
 
-        openButton.addEventListener('click', () => this.toggleState(chatBox))
+        openButton.addEventListener('click', () => this.toggleState(chatBox)) // click open button, then execute this
 
-        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox)) // sending message 
 
         const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
+        node.addEventListener("keyup", ({key}) => {     // keyup-> hit enter button will send message
             if (key === "Enter") {
                 this.onSendButton(chatBox)
             }
@@ -30,7 +30,7 @@ class Chatbox {
 
         // show or hides the box
         if(this.state) {
-            chatbox.classList.add('chatbox--active')
+            chatbox.classList.add('chatbox--active') // this is connected to CSS file -> pretty
         } else {
             chatbox.classList.remove('chatbox--active')
         }
@@ -40,43 +40,44 @@ class Chatbox {
         var textField = chatbox.querySelector('input');
         let text1 = textField.value
         if (text1 === "") {
-            return;
+            return;        // when its empty, do nothing
         }
 
         let msg1 = { name: "User", message: text1 } /* text input -> must use same key -> "message" */
         this.messages.push(msg1);
         
         // 원래 이 자리에 이거 있었음 'http://127.0.0.1:5000/predict -> local host'
-        fetch( input: $SCRIPT_ROOT + '/predict', init:{  // predict function in app.py   // SCRIPT_ROOT is defined in HTML bottom!!!
-            method: 'POST',
+        fetch( $SCRIPT_ROOT + '/predict', {  // predict function in app.py   // SCRIPT_ROOT is defined in HTML bottom!!!
+            method: 'POST', // post method
             body: JSON.stringify({ message: text1 }), // make it as string
-            mode: 'cors',
+            mode: 'cors', 
             headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'application/json' // specify the file as json
             },
           })
-          .then(r => r.json())
+        // this is end of sending request. Need to get response
+          .then(r => r.json()) // extract json
           .then(r => {
-            let msg2 = { name: "Sam", message: r.answer }; // this time bot will answer (bot name is Sam)
-            this.messages.push(msg2); // from response result 
+            let msg2 = { name: "Sam", message: r.answer };  // this time bot will answer (bot name is Sam)
+            this.messages.push(msg2);     // from response result -> app.py predict function
             this.updateChatText(chatbox) // update (separate function)
             textField.value = ''
 
-        }).catch((error) => {
+        }).catch((error) => {   // if errir appears, then gives nothing
             console.error('Error:', error);
             this.updateChatText(chatbox)
             textField.value = ''
           });
     }
 
-    updateChatText(chatbox) {
+    updateChatText(chatbox) { // updating function for chatbot
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
-            {
+            if (item.name === "Sam") // if its chat bot
+            { // define visiter section
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
-            else
+            else // otherwise define operator section
             {
                 html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
             }
