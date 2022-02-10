2add3508ff317fc1c5abbc62f6befc2476f132d6
Joseph <59439026+JosephKBS@users.noreply.github.com>
2022-02-09 21:37:44 -0500

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -4,12 +4,12 @@ from chat import get_response
 
 app = Flask(__name__)
 
-CORS(app)
+#CORS(app)
 # 만약 CORS(app) 실행할거면 밑에 app.get전부 지우면 됨.
 
-#@app.get("/")
-#def index_get():
-#    return render_template("base.html")
+@app.get("/")
+def index_get():
+    return render_template("base.html")
 
 @app.post("/predict"):
 def predict():
